214a5b4f1f89c434e53fa97a3a653571006023e6
Cedric LE CAM <cedric.lecam@sii.fr>
2025-01-03 12:33:26 +0100

    Mise à jour du copyright.

--- src/Learning/UPDataPreparation/UPFrequencyVector.cpp
+++ src/Learning/UPDataPreparation/UPFrequencyVector.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Orange. All rights reserved.
+// Copyright (c) 2025 Orange. All rights reserved.
 // This software is distributed under the BSD 3-Clause-clear License, the text of which is available
 // at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
 
@@ -39,7 +39,3 @@ int UPDenseFrequencyVector::ComputeTotalFrequency() const
 
 ////////////////////////////////////////////////////////////////////
 // Classe KWFrequencyTable
-
-
-
-
