19263a3264a8acafa950404e351887eaaf2b71a8
Tristan Debrousse <tristan.debrousse.ext@orange.com>
2025-12-10 12:03:43 +0100

    Fix filename case in #include

--- src/Learning/UPDataPreparation/UPAttributeStats.cpp
+++ src/Learning/UPDataPreparation/UPAttributeStats.cpp
@@ -3,7 +3,7 @@
 // at https://spdx.org/licenses/BSD-3-Clause-Clear.html or see the "LICENSE" file for more details.
 
 #include "UPAttributeStats.h"
-#include "UpLearningSpec.h"
+#include "UPLearningSpec.h"
 #include "UPFrequencyVector.h"
 #include "UPGrouperUMODL.h"
 
